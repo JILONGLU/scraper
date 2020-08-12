7a28c91d64f79947c85005285a1352c2e535d5c7
Brian Chang <changbhc@gmail.com>
2020-08-11 23:08:18 -0700

    add constant to fetch webpage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var cheerio = require("cheerio");
+
+const siteUrl =
+  "http://ets.aeso.ca/ets_web/ip/Market/Reports/ActualForecastWMRQHReportServlet";
+const axios = require("axios");
+
+const fetchData = new Promise((resolve, reject) => {
+  axios
+    .get(siteUrl)
+    .then((response) => {
+      var result = response.data;
+      resolve(cheerio.load(result));
+    })
+    .catch((error) => {
+      reject(error);
+    });
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
