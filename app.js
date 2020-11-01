dea329f9082aa860a9bae153df17040dc6b92518
Brian Chang <changbhc@gmail.com>
2020-10-31 19:55:45 -0700

    move all the logic into app.js

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ require("dotenv").config({ path: __dirname + "/.env" });
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
+const second = 1000;
+const minute = 60 * second;
+const hour = 60 * minute;
+const {
+  getDateAndPrices,
+  findValidEntries,
+  parseDataAndSend,
+} = require("./helpers.js");
+
+// var indexRouter = require("./routes/index");
 
 var app = express();
 
@@ -16,10 +24,9 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+// app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -40,6 +47,7 @@ app.use(function (err, req, res, next) {
 var port = 9000;
 
 app.listen(process.env.PORT || port, () => {
+  setInterval(parseDataAndSend, 1 * hour);
   console.log(`App listening on port ${port}`);
 });
 
